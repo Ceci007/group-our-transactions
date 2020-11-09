49a59ed0bc120827c72adb7c6b8eff7aef17a897
solbencas <cecibenitezca@gmail.com>
2020-11-09 11:57:23 -0300

    Generate User  model and User controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.hosts << "68179a24393d43c79646b4a60d04f9cb.vfs.cloud9.us-east-2.amazonaws.com"
 end
