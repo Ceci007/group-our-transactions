7a246c6071b627a703e2dfb8049592779aa79630
solbencas <cecibenitezca@gmail.com>
2020-11-09 12:29:52 -0300

    Add create action on users controller and sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def new
+  before_action :set_user, only: [:show]
+  def new; end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
+    flash[:notice] = "Welcome #{@user.username}"
+    redirect_to user_path(@user)
+  end
+
+  def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :users, except: [:new]
   root 'users#new'
 end
