a53b0b417a266ea1a1feaecdc47a2aa31e7515a2
solbencas <cecibenitezca@gmail.com>
2020-11-10 18:42:18 -0300

    remove logic in decor and category views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,20 @@ module ApplicationHelper
     category_ids = decor.categories.pluck(:category_id)
     Category.all.reject { |value| category_ids.include?(value.id) }
   end
+
+  def icon_display(decor)
+    if decor.categories.empty?
+      fa_icon 'times', class: 'fa-3x'
+    else
+      fa_icon decor.categories.first.icon, class: 'fa-3x'
+    end
+  end
+
+  def decor_errors_msgs(decor)
+    render 'decors/error_msgs' if decor.errors.full_messages.any?
+  end
+
+  def category_errors_msgs(category)
+    render 'categories/error_msgs' if category.errors.any?
+  end
 end
