69c4185810933b8311e83d278ed2ff6cb34c6d98
solbencas <cecibenitezca@gmail.com>
2020-11-11 17:52:50 -0300

    Add categories controller spec test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,57 @@
-require 'rails_helper'
+class CategoriesController < ApplicationController
+  before_action :require_login
+  before_action :set_category, only: %i[show edit update]
+  before_action :require_user, only: %i[edit update]
+  before_action :icons_array, only: %i[new create edit update]
 
-RSpec.describe UsersController, type: :controller do
-  describe 'User #create' do
-    context 'it redirects to user#show when successful' do
-      it 'redirects to the user show page' do
-        post :create, params: { user: { username: 'Ceci' } }
-        expect(response).to redirect_to(user_url(User.find_by(username: 'Ceci')))
-      end
-    end
+  def new
+    @category = Category.new
+  end
 
-    context 'it render to new template when unsuccessful' do
-      it 'render new template' do
-        post :create, params: { user: { username: '' } }
-        expect(response).to render_template(:new)
-      end
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      flash[:notice] = 'You successfully created a new category.'
+      redirect_to categories_path
+    else
+      render 'new'
     end
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category was successfully edited.'
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @categories = Category.all.alphabetical_order.includes(:user)
+  end
+
+  def show
+    @decors = @category.decors.includes(:user)
+    @total = @decors.pluck(:price).sum
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def icons_array
+    @category_array = icons
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+
+  def require_user
+    redirect_to categories_path if current_user != @category.user
   end
 end
---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :controller do
+  let(:user) { User.create(username: 'Ceci', email: 'cecibenitezca@gmail.com') }
+  
+  describe 'Category #create' do
+    before(:example) do
+      session[:user_id] = user.id
+    end
+
+    context 'it redirects to categories#index when successful' do
+      it 'redirects to the categories index page' do
+        post :create, params: { category: { name: 'Bathroom', icon: 'bath' } }
+        expect(response).to redirect_to(categories_path)
+      end
+    end
+
+    context 'it render to new template when unsuccessful' do
+      it 'render new template' do
+        post :create, params: { category: { name: '', icon: 'bath' } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'Category #update' do
+    before(:example) do
+      session[:user_id] = user.id
+      @category = user.categories.create(name: 'Bathroom', icon: 'bath')
+    end
+    context 'it redirects to categories#index when successful' do
+      it 'redirects to the categories index page' do
+        patch :update, params: { id: @category.id, category: { name: 'Bathroom decor', icon: 'bath' } }
+        expect(response).to redirect_to(categories_path)
+      end
+    end
+
+    context 'it render to edit template when unsuccessful' do
+      it 'render new template' do
+        patch :update, params: { id: @category.id, category: { name: '', icon: 'bath' } }
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'category #index' do
+    before(:example) do
+      session[:user_id] = user.id
+    end
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'category #show' do
+    before(:example) do
+      session[:user_id] = user.id
+    end
+    it 'renders show template' do
+      @category = user.categories.create(name: 'Bathroom', icon: 'bath')
+      get :show, params: { id: @category.id }
+      expect(response).to render_template(:show)
+    end
+  end
+end
