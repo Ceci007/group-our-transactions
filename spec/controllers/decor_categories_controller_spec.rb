c491cd2ec81bba3b54cd4312c41ce412fa981f74
solbencas <cecibenitezca@gmail.com>
2020-11-11 17:17:26 -0300

    Add test for decor categories controller

---
+++ spec/controllers/decor_categories_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe DecorCategoriesController, type: :controller do
+  let(:user) { User.create(username: 'Ceci', email: 'cecibenitezca@gmail.com') }
+
+  describe 'DecorCategory #create' do
+    before(:example) do
+      session[:user_id] = user.id
+      @decor = user.decors.create(name: 'Towel', price: 4)
+      @category = user.categories.create(name: 'Bathroom', icon: 'bath')
+    end
+    context 'it redirects to user_decors#index when successful' do
+      it 'redirects to the user_decors index page' do
+        post :create, params: { user_id: user.id, decor_id: @decor.id, decor_category: { category_id: @category.id } }
+        expect(response).to redirect_to(user_decors_path)
+      end
+    end
+  end
+end
