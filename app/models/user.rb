5b254ff0a951eea6c6b5c4aac442aacc80d46b16
solbencas <cecibenitezca@gmail.com>
2020-11-13 16:19:54 -0300

    Add new validation for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 105 }
 
   has_many :decors, dependent: :destroy
   has_many :categories
